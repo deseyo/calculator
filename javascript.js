eee60c02dfa87db5dfacdb343f71bbc89177accc
deseyo <156849439+deseyo@users.noreply.github.com>
2024-05-24 09:10:57 +0200

    Add operate function that runs one of the math operations functions depending on user input

--- javascript.js
+++ javascript.js
@@ -2,6 +2,21 @@ let inputNumOne;
 let inputOperator;
 let inputNumTwo;
 
+function operate(numOne, operator, numTwo) {
+  switch (operator) {
+    case ("+"):
+      return add(+numOne, +numTwo);
+    case ("-"):
+      return subtract(numOne, numTwo);
+    case ("*"):
+      return multiply(numOne, numTwo);
+    case ("/"):
+      return divide(numOne, numTwo);
+    default:
+      return "Error, wrong input";
+  }
+}
+
 function add(numOne, numTwo) {
   return numOne + numTwo;
 }
