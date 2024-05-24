f6d43253266dfc4f15b05a40c7dc44d8551c6992
deseyo <156849439+deseyo@users.noreply.github.com>
2024-05-24 19:52:32 +0200

    Add event listeners for num buttons to display user input and depending on nums turn user input is added to one of two variables

--- javascript.js
+++ javascript.js
@@ -1,6 +1,30 @@
-let inputNumOne;
-let inputOperator;
-let inputNumTwo;
+const displayField = document.querySelector("#calculator-display");
+const btnNumZero = document.querySelector("#btn-num-zero");
+const btnNumOne = document.querySelector("#btn-num-one");
+const btnNumTwo = document.querySelector("#btn-num-two");
+const btnNumThree = document.querySelector("#btn-num-three");
+const btnNumFour = document.querySelector("#btn-num-four");
+const btnNumFive = document.querySelector("#btn-num-five");
+const btnNumSix = document.querySelector("#btn-num-six");
+const btnNumSeven = document.querySelector("#btn-num-seven");
+const btnNumEight = document.querySelector("#btn-num-eight");
+const btnNumNine = document.querySelector("#btn-num-nine");
+
+addBtnEventListener(btnNumZero, 0);
+addBtnEventListener(btnNumOne, 1);
+addBtnEventListener(btnNumTwo, 2);
+addBtnEventListener(btnNumThree, 3);
+addBtnEventListener(btnNumFour, 4);
+addBtnEventListener(btnNumFive, 5);
+addBtnEventListener(btnNumSix, 6);
+addBtnEventListener(btnNumSeven, 7)
+addBtnEventListener(btnNumEight, 8);
+addBtnEventListener(btnNumNine, 9);
+
+let numsTurn = 1;
+let inputNumOne = "";
+let inputOperator = "";
+let inputNumTwo = "";
 
 function operate(numOne, operator, numTwo) {
   switch (operator) {
@@ -31,4 +55,12 @@ function multiply(numOne, numTwo) {
 
 function divide(numOne, numTwo) {
   return numOne / numTwo;
+}
+
+function addBtnEventListener(btn, num) {
+  btn.addEventListener("click", () => {
+    displayField.textContent += num;
+    if (numsTurn === 1) inputNumOne += num;
+    else if (numsTurn === 2) inputNumTwo += num;
+  });
 }
